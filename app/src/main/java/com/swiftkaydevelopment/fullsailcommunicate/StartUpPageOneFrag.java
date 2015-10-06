0147b7652deef13a10fda64b210c4c511ddde382
khaines178 <khaines178@gmail.com>
2015-10-06 02:57:31 -0400

    Initial commit

---
+++ app/src/main/java/com/swiftkaydevelopment/fullsailcommunicate/StartUpPageOneFrag.java
@@ -0,0 +1,26 @@
+package com.swiftkaydevelopment.fullsailcommunicate;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by khaines178 on 9/19/15.
+ */
+public class StartUpPageOneFrag extends Fragment {
+
+    public StartUpPageOneFrag() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View layout = inflater.inflate(R.layout.fragment_main,container,false);
+        return layout;
+    }
+}
